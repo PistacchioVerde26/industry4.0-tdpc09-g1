281e6ffa046395121e631c9ad94b3d03a0670676
Shapeinabox <shapeinabox@gmail.com>
2018-05-21 23:37:18 +0200

    aggiunte classi model

---
+++ industry4-camerana-gruppo1/App_Code/Prodotto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrizione di riepilogo per Prodotto
+/// </summary>
+public class Prodotto {
+    public Prodotto() {
+        //
+        // TODO: aggiungere qui la logica del costruttore
+        //
+    }
+}
---
+++ industry4-camerana-gruppo1/App_Code/Utente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrizione di riepilogo per Utente
+/// </summary>
+public class Utente {
+    public Utente() {
+        //
+        // TODO: aggiungere qui la logica del costruttore
+        //
+    }
+}
