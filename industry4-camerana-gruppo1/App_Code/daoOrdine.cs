15606c0d3842f3debe139a9b1ef42b4449d95e92
TDPC09-08 <shapeinabox@gmail.com>
2018-05-22 21:14:17 +0200

    prova daoOrdine

--- industry4-camerana-gruppo1/App_Code/daoOrdine.cs
+++ industry4-camerana-gruppo1/App_Code/daoOrdine.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +9,23 @@ using System.Web;
 /// Descrizione di riepilogo per daoOrdine
 /// </summary>
 public class daoOrdine {
+
     public daoOrdine() {
         //
         // TODO: aggiungere qui la logica del costruttore
         //
     }
+
+    public Ordine GetOrdine(int ID) {
+
+        DbEntity db = new DbEntity();
+
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "SELECT Ordini.*, Lavorazioni.*, TipoLavorazione.* FROM Ordini INNER JOIN Lavorazioni ON Ordini.idordine = Lavorazioni.fkordine INNER JOIN TipoLavorazione ON Lavorazioni.fk_tipolav = TipoLavorazione.idtipolav WHERE Ordini.idordine=" + ID;
+
+
+        return null;
+    }
+
 }
