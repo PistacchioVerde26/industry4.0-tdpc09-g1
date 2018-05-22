c9b59928b0990a4624db14938c31f817e0bfc3ed
TDPC09-08 <shapeinabox@gmail.com>
2018-05-22 20:49:18 +0200

    aggiunte alcune classi dao

---
+++ industry4-camerana-gruppo1/App_Code/Lavorazione.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrizione di riepilogo per Lavorazione
+/// </summary>
+public class Lavorazione {
+
+    public int ID { get; set; }
+
+    public string Tipo { get; set; }
+    public string Dettagli { get; set; }
+    public DateTime Inizio { get; set; }
+    public DateTime Fine { get; set; }
+    public int Stato { get; set; }
+    public string Opzione { get; set; }
+
+    public Lavorazione() {
+        //
+        // TODO: aggiungere qui la logica del costruttore
+        //
+    }
+}
---
+++ industry4-camerana-gruppo1/App_Code/daoUtente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrizione di riepilogo per daoUtente
+/// </summary>
+public class daoUtente {
+    public daoUtente() {
+        //
+        // TODO: aggiungere qui la logica del costruttore
+        //
+    }
+}
