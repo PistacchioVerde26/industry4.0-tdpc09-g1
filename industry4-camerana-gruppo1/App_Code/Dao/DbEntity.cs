3c99937c8ea527d1adad3a97b74499e3a1cb9825
Shapeinabox <shapeinabox@gmail.com>
2018-05-25 15:31:13 +0200

    commentato using e aggiunto Web.config ai file ignorati

--- industry4-camerana-gruppo1/App_Code/Dao/DbEntity.cs
+++ industry4-camerana-gruppo1/App_Code/Dao/DbEntity.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
+//using System.Linq;
 using System.Web;
 
 /// <summary>
