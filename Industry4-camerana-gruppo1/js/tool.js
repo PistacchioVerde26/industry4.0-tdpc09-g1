1a2b304aed7ac92aad324c788356185725ffc603
Shapeinabox <shapeinabox@gmail.com>
2018-06-03 00:25:53 +0200

    prova immagine dinamica da ultimare

---
+++ Industry4-camerana-gruppo1/js/tool.js
@@ -0,0 +1,8 @@
+﻿
+
+//drp_Materiale
+
+function ChangeMateriale(select) {
+    var value = select.options[select.selectedIndex].text
+    $("#manico").css("background-image", "url(../imgs/mat-metal.jpg)");
+}
